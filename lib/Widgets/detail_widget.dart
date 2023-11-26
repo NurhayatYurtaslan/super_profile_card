83f6d4c95e41deb05fd6cd2404df515e86ec15b3
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-26 19:02:50 +0300

    add ✨: widget

---
+++ lib/Widgets/detail_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+Widget detailWidget({required IconData icon, required String text}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 30),
+    child: Column(
+      children: [
+        Row(
+          children: [
+            Icon(
+              icon,
+              size: 30,
+              color: Colors.white70,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Text(
+                text,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const Divider(),
+      ],
+    ),
+  );
+}
