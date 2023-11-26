fb6b48c03c255523f664568f3e8e985deb351e07
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-26 19:01:52 +0300

    update 🆕: homeview file

--- example/lib/homeview.dart
+++ example/lib/homeview.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:super_profile_card/profile_card.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Super Profile Card"),
+      ),
+      body: const SuperProfileCard(),
+    );
   }
 }
