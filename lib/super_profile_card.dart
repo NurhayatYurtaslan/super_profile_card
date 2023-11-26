2b54b98001d4443db364b53d30c97fa8831a0374
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-26 19:02:30 +0300

    update 🆕: package file

--- lib/super_profile_card.dart
+++ lib/super_profile_card.dart
@@ -1,7 +1,4 @@
 library super_profile_card;
 
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:super_profile_card/profile_card.dart';
